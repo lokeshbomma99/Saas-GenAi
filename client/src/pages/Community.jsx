60ced66008bc256cb350a083218c48194a799460
lokeshbomma99 <169030699+lokeshbomma99@users.noreply.github.com>
2025-11-12 23:10:02 +0530

    Update Community.jsx

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -5,7 +5,7 @@ import { Heart } from 'lucide-react';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-// axios.defaults.baseURL - import.meta.env.VITE_BASE_URL;
+axios.defaults.baseURL - import.meta.env.VITE_BASE_URL;
 
 const Community = () => {
   const [creations, setCreations] = useState([]);
