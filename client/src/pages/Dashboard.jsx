910b04954b76d383e7ce6c97dcce9d5d1dd8e94f
lokeshbomma99 <169030699+lokeshbomma99@users.noreply.github.com>
2025-11-12 23:10:23 +0530

    Set default axios base URL from environment variable

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -6,6 +6,7 @@ import CreationItem from '../components/CreationItem';
 import axios from 'axios'
 import toast from 'react-hot-toast';
 
+axios.defaults.baseURL - import.meta.env.VITE_BASE_URL;
 
 const Dashboard = () => {
     const [creations, setCreations] = useState([]);
