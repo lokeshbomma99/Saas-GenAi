68791d1f691526a3fafd6e1cbbbff230f25d114b
lokeshbomma99 <169030699+lokeshbomma99@users.noreply.github.com>
2025-11-13 00:07:52 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -31,7 +31,7 @@ const Sidebar = ({ sidebar, setSidebar }) => {
   return (
     <div
       className={`w-60 bg-white border-r border-gray-200 flex flex-col justify-between items-center
-    max-sm:absolute top-14 bottom-0 ${sidebar ? 'translate-x-0' : 'max-sm:-translate-x-full'}
+    max-sm:absolute top-14 bottom-0 z-40 ${sidebar ? 'translate-x-0' : 'max-sm:-translate-x-full'}
     transition-all duration-300 ease-in-out`}
     >
       <div className="my-7 w-full">
